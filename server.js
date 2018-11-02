58df5e20baf3a8dc5acc077347227bdc2187eeef
Christian <cackren@gmail.com>
2018-11-02 22:01:39 +0200

    Installed the hbs module (node wrapper for handlebars.js)

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 var app = express();
 
+// Add some middle-ware to serve static files
+// The __dirname variable gives the path to the project directory
+app.use(express.static(__dirname + '/public'));
+
 // Set up GET request with req (request) and res (response) arguments
 app.get('/', (req, res) => {
     //res.send('<h1>Hello from express!</h1>');
@@ -26,4 +30,6 @@ app.get('/bad', (req, res) => {
 });
 
 // Bind the express app to specific port
-app.listen(3030);
+app.listen(3030, () => {
+    console.log('Express server is up on port 3030');
+});
